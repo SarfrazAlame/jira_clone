a80ef347cc4898eec95a33f6be2a6be093c3694a
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-10-22 11:36:55 +0530

    navbar for movile

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -40,15 +40,17 @@ export const Navigation = () => {
     <ul className="flex flex-col ">
       {routes.map((item) => {
         const isActive = false;
-        const Icon = isActive ? item.icon : item.activeIcon;
+        const Icon = isActive ? item.activeIcon : item.icon;
 
         return (
           <Link key={item.label} href={item.href}>
-            <div className={cn(
-              "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
-              isActive && "bg-white shadow-sm hover:opacity-100 text-primary",
-            )}>
-              <Icon className="size-5 text-neutral-500"/>
+            <div
+              className={cn(
+                "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
+                isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+              )}
+            >
+              <Icon className="size-5 text-neutral-500" />
               {item.label}
             </div>
           </Link>
