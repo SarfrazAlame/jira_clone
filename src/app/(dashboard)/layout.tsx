b751fd20b738c499c1fcc970a70d11f2f5a3e8b6
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-10-22 06:08:29 +0530

    sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ const DashboadLayout = ({ children }: DashboadLayoutProps) => {
     <div className="min-h-screen">
       <div className="flex w-full h-full">
         <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-auto">
-            <Sidebar/>
+          <Sidebar />
         </div>
         <div className="lg:pl-[264px]">
           <div className="mx-auto max-w-screen-2xl h-full">
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import { DottedSeparator } from "./dotted-separator";
+import Navigation from "./Navigation";
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href={"/"}>
+        <Image src="/logo.svg" alt="" width={164} height={48} />
+      </Link>
+      <DottedSeparator className="my-4" />
+      <Navigation />
+    </aside>
+  );
 };
